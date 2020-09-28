03601f4f7831e4e803689eec4af30a40cc7da9d7
Alberto Basalo <albertobasalo@hotmail.com>
2020-09-28 13:28:38 +0200

    chore: imports from ngx-google-analytics

--- projects/ab-geo-app/src/app/app.module.ts
+++ projects/ab-geo-app/src/app/app.module.ts
@@ -1,11 +1,20 @@
 import { NgModule } from '@angular/core';
+import {
+  NgxGoogleAnalyticsModule,
+  NgxGoogleAnalyticsRouterModule,
+} from 'ngx-google-analytics';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [AppRoutingModule, CoreModule],
+  imports: [
+    AppRoutingModule,
+    CoreModule,
+    NgxGoogleAnalyticsModule.forRoot('UA-83549527-1'),
+    NgxGoogleAnalyticsRouterModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
